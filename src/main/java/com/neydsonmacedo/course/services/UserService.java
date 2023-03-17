a7927c707a5d46eab60a7fbfaddb29147dd940d3
neydsonmacedo <neydson_macedo@hotmail.com>
2023-03-17 16:54:40 -0300

    Service layer, component registration

---
+++ src/main/java/com/neydsonmacedo/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.neydsonmacedo.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.neydsonmacedo.course.entities.User;
+import com.neydsonmacedo.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+
+}
