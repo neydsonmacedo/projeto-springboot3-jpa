e94509b14216033e338d9476b70fa7e92adcc824
neydsonmacedo <neydson_macedo@hotmail.com>
2023-03-18 11:31:09 -0300

    Category

---
+++ src/main/java/com/neydsonmacedo/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.neydsonmacedo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.neydsonmacedo.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category,Long>{
+	
+	
+}
